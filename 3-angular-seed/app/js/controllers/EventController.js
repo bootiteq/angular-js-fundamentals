c41644e039edd908a2d61599d8e05907b3b896db
Ben Bowes <bb@benbowes.com>
2014-05-07 20:52:13 +1000

    end of lesson handling events

--- 3-angular-seed/app/js/controllers/EventController.js
+++ 3-angular-seed/app/js/controllers/EventController.js
@@ -19,24 +19,35 @@ eventsApp.controller('EventController',
 					creatorName: "Fangio Bertolli",
 					duration: "2 hours",
 					level: "Advanced",
-					abstract: "Nunc libero nibh, suscipit eu mollis nec, porttitor sit amet eros. Duis ac porta eros. Quisque sit amet ipsum sit amet augue commodo auctor.  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+					abstract: "Nunc libero nibh, suscipit eu mollis nec, porttitor sit amet eros. Duis ac porta eros. Quisque sit amet ipsum sit amet augue commodo auctor.  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+					upVoteCount: 0
 				},
 				{
 					name: "As good as a session can get I feel sure",
 					creatorName: "Fangio Bertolli",
 					duration: "1 hour",
 					level: "Advanced",
-					abstract: "Nulla non nisl ut tellus dignissim dapibus. Sed in urna ut dui interdum rutrum. Praesent quis turpis id tellus blandit blandit porta quis libero. Vivamus fermentum elementum orci, vel suscipit leo ultricies id. Donec fermentum nulla ornare risus auctor, ut mattis lacus ornare."
+					abstract: "Nulla non nisl ut tellus dignissim dapibus. Sed in urna ut dui interdum rutrum. Praesent quis turpis id tellus blandit blandit porta quis libero. Vivamus fermentum elementum orci, vel suscipit leo ultricies id. Donec fermentum nulla ornare risus auctor, ut mattis lacus ornare.",
+					upVoteCount: 0
 				},
 				{
 					name: "This session sucks a bit but you'd now have a few buddies",
 					creatorName: "Fangio Bertolli",
 					duration: "1 hour",
 					level: "Advanced",
-					abstract: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sagittis massa dui, eu egestas tellus bibendum quis. Sed eu nisl erat."
+					abstract: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sagittis massa dui, eu egestas tellus bibendum quis. Sed eu nisl erat.",
+					upVoteCount: 0
 				}
 			]
-		}
+		};
+
+		$scope.upVoteSession = function(session){
+			session.upVoteCount++
+		};
+
+		$scope.downVoteSession = function(session){
+			session.upVoteCount--;
+		};
 
 	}
 );
