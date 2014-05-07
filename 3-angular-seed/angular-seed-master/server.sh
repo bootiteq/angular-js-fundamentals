4e9e771f6afa00a67a327677f066024ab4b55321
Ben Bowes <bb@benbowes.com>
2014-05-07 19:39:19 +1000

    Change directory structure 2

--- 3-angular-seed/angular-seed-master/server.sh
+++
@@ -1,2 +0,0 @@
-cd ~/Dropbox/ME/GIT/angular-js-fundamentals/3-angular-seed/angular-seed-master/app/
-node ../scripts/web-server.js
--- 3-angular-seed/server.sh
+++ 3-angular-seed/server.sh
@@ -1,2 +1,2 @@
-cd ~/Dropbox/ME/GIT/angular-js-fundamentals/3-angular-seed/angular-seed-master/app/
+cd ~/Dropbox/ME/GIT/angular-js-fundamentals/3-angular-seed/app/
 node ../scripts/web-server.js
